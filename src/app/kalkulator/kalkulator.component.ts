ec5463db31ab86c7417997707f1c30db971a8f5a
Piotr Marecki <pm@aimachina.io>
2019-11-16 15:00:41 +0100

    kwadraty

--- src/app/kalkulator/kalkulator.component.ts
+++ src/app/kalkulator/kalkulator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-kalkulator',
@@ -7,12 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KalkulatorComponent implements OnInit {
   wynik = 12;
-  liczbaA = 0;
-  liczbaB = 0;
+  liczbaA: string;
+  liczbaB: string;
+  kwadrat = 0;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  computeSum() {
+    const a = parseInt(this.liczbaA);
+    const b = parseInt(this.liczbaB);
+    this.wynik = a + b;
+    this.kwadrat = this.nearestSquare(this.wynik);
+
+  }
+
+  /**
+   *
+   * @param value Wartość parametru dla którego liczymy kwadraty
+   * @returns Najmnijesza liczba całkowita x = y * y >= value, gdzie y to l. całkowita
+   */
+  nearestSquare(value: number): number {
+    for (let y = 0; y <= 100; y++) {
+      let x = y * y;
+      if (x >= value) return x;
+    }
+
+    // Działanie instrukcji warunkowych...
+    // if (value < 100) {
+    //   //kod wykonany jeśli wartość "value" < 100
+    //   return 50;
+    // } else {
+    //   //kod wykonany jeśli wartość "value" >= 100
+    //   return 200;
+    // }
+  }
 }

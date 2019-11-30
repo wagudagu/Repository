f4cac0948f7bdd3f58ea0609764afbf21e0e31df
Piotr Marecki <pm@aimachina.io>
2019-11-30 13:29:31 +0100

    add guess game

--- src/app/wonder-view/wonder-view.component.ts
+++ src/app/wonder-view/wonder-view.component.ts
@@ -6,11 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wonder-view.component.less']
 })
 export class WonderViewComponent implements OnInit {
-  liczba: string;
+  inputtext: string;
+  wynik: number;
+
+  guess: string;
+  wynikSprawdzenia: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  dodaj() {
+    //ten kod będzie wykonany po przyciśnięciu odpowiedniego buttona
+    let nnn = parseInt(this.inputtext);
+///
+    this.wynik = nnn + nnn;
+
+  }
+
+  check() {
+    let nnn = parseInt(this.guess);
+    if (nnn == 500) {
+      //co wykonać jeśli trafiliśmy liczbę!
+    }
+    if (nnn > 500) {
+      //co ma być wykonane jeśli nnn > 500
+      this.wynikSprawdzenia = 'poszukiwana liczba jest mniejsza od ' + nnn;
+    } else {
+      //co wykonać w przeciwnym wypadku
+      this.wynikSprawdzenia = 'poszukiwana liczba jest większa od ' + nnn;
+    }
+  }
 }
